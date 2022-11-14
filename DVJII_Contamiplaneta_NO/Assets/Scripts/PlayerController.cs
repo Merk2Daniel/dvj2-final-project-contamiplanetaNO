6886f3f4aa48942f33c57ce71bdb77d7800903cd
Daniel Arnaldo Mercado <merk2daniel@gmail.com>
2022-11-14 17:58:18 -0300

    fixes

--- DVJII_Contamiplaneta_NO/Assets/Scripts/PlayerController.cs
+++ DVJII_Contamiplaneta_NO/Assets/Scripts/PlayerController.cs
@@ -4,39 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header ("Movement")] //AGREGADO
+
     // Move object using accelerometer
     private float speed = 10.0f;
+    public bool moveIsActivated; //AGREGADO //Para deshabilitar el movimiento cuando el temporizador (si lo ponemos) llegue a 0 o en alguna otra situación especial
+
+    [Header ("Animator")] //AGREGADO
+    public Animator anim; //AGREGADO
+    private SpriteRenderer theSR; //AGREGADO
+
+    void Start() //AGREGADO
+    {
+        anim = GetComponent<Animator>(); //AGREGADO
+        theSR = GetComponent<SpriteRenderer>(); //AGREGADO
+    }
 
     void Update()
     {
         Vector3 dir = Vector3.zero;
 
-        dir.x = Input.acceleration.x;
+        dir.x = Input.acceleration.x;/*Input.GetAxis("Horizontal");*/
         dir.y = Input.acceleration.y;
         dir.z = Input.acceleration.z;
 
         if (dir.x < 0.2 && dir.x > - 0.2)
         {
-            GetComponent<Animator>().SetBool("walking", false);
+            anim.SetBool("walking", false);
             dir.x = 0;
         }
 
         if (dir.x < - 0.2)
         {
-            if (!GetComponent<SpriteRenderer>().flipX)
-            {
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
-            GetComponent<Animator>().SetBool ("walking",true);
+            transform.localScale = new Vector3(-1,1,1);
+            anim.SetBool ("walking",true);
         }
 
         if (dir.x > 0.2)
         {
-            if (GetComponent<SpriteRenderer>().flipX)
-            {
-                GetComponent<SpriteRenderer>().flipX = false;
-            }
-            GetComponent<Animator>().SetBool("walking", true);
+            transform.localScale = new Vector3(1,1,1);
+            anim.SetBool("walking", true);
         }
 
         // clamp acceleration vector to unit sphere
